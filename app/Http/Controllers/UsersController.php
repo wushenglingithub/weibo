0897e085a4b0e8563649bec4ceb7189003530438
wsl <450176213@qq.com>
2020-05-22 11:20:31 +0800

    增加csrf验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,4 +14,21 @@ class UsersController extends Controller
     public function show(User $user){
         return view('users.show',compact('user'));
     }
+
+    /**
+     * required是否空
+     * unique唯一性：模型表
+     * max、min 最大、最小
+     *confirmed验证两次密码是否一致
+     * @param Request $request
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }

5eecba0f693481061e7187dd5b774453ec67d6f2
wsl <450176213@qq.com>
2020-05-22 15:31:13 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,12 +7,12 @@
 <body>
     @include('layouts._header')
 
-
-<div class="container">
-    <div class="offset-md-1 col-md-10">
-        @yield('content')
-        @include('layouts._footer')
+    <div class="container">
+        <div class="offset-md-1 col-md-10">
+            @include('shared._messages')
+            @yield('content')
+            @include('layouts._footer')
+        </div>
     </div>
-</div>
 </body>
 </html>

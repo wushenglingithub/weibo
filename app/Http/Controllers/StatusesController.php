e16cd383d24994c9b3d52b062f9177380459bed6
vagrant <vagrant@homestead>
2020-05-27 08:43:14 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -25,4 +25,20 @@ class StatusesController extends Controller
         session()->flash('success', '发布成功！');
         return redirect()->back();
     }
+
+    /**
+     * @param Status $status
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function destroy(Status $status)
+    {
+        //自动查找并注入对应 ID 的实例对象 $status，
+        //如果找不到就会抛出异常
+        $this->authorize('destroy', $status);
+        //删除授权的检测，不通过会抛出 403 异常
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
+        return redirect()->back();
+    }
 }

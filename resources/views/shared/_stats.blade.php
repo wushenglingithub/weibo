6d7e9d79a6a456c80e4cd88469271ee9f9fb119b
vagrant <vagrant@homestead>
2020-05-27 10:10:46 +0000

    关注和粉丝列表页面

--- resources/views/shared/_stats.blade.php
+++ resources/views/shared/_stats.blade.php
@@ -1,19 +1,17 @@
-<a href="#">
+<a href="{{ route('users.followings', $user->id) }}">
     <strong id="following" class="stat">
         {{ count($user->followings) }}
     </strong>
     关注
 </a>
-<a href="#">
+<a href="{{ route('users.followers', $user->id) }}">
     <strong id="followers" class="stat">
         {{ count($user->followers) }}
     </strong>
     粉丝
 </a>
-<a href="#">
+<a href="{{ route('users.show', $user->id) }}">
     <strong id="statuses" class="stat">
-        {{--最佳实践应采用的方法是在数据库中添加一个模型计数器字段，
-        在每次对模型进行创建或删除时对该字段进行更新--}}
         {{ $user->statuses()->count() }}
     </strong>
     微博
